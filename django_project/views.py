43acdcd169b70715d6287e7201e4375072c3c2be
David Ouma <119806895+DaveOuma@users.noreply.github.com>
2024-04-24 12:56:58 +0000

    Integrated Apis that generates random content

--- django_project/views.py
+++ django_project/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
-  return render(request, 'templates/index.html')
+  r1 = requests.get('https://api.github.com/events')
+  data = r1.json()
+  events = data[0]['repo']
+  
+  r2 = requests.get('https://www.boredapi.com/api/activity')
+  data = r2.json()
+  activity  = data['activity']
+
+  r3 = requests.get('https://dog.ceo/api/breeds/image/random')
+  res3 = r3.json()
+  dog = res3['message']
+  
+  return render(request, 'templates/index.html', {'events': events, 'activity': activity, 'dog': dog})
