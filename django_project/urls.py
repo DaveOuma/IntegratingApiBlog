fc8e48a13b856434d96c00a5fcb7e28a0c4c1496
Chebichii-Lab <chebichiiserem@gmail.com>
2024-04-27 11:40:05 +0300

    added the url path to the home page

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,6 +20,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('index/', views.index, name='index'),
+    path('refresh-data/', views.refresh_data, name='refresh_data'),
   
 ]
